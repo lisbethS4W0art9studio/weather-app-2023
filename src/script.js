841a42e18b6b120e0f2c8a6d0d3a2eafb70010e5
Lisbeth Stafferton <131831523+lisbethS4W0art9studio@users.noreply.github.com>
2023-08-05 14:12:47 +1000

    Updated script with axios and function
    apiUrl, apiKey and function displayTemperature

--- src/script.js
+++ src/script.js
@@ -1,1 +1,8 @@
+function displayTemperature(response) {
+  console.log(response.data.main.temp);
+}
+
 let apiKey = "46bcab46c54344b46e2378407235d572";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
