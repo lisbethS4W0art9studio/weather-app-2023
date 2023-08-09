2108ba4910f3b91dad0a38cefa74fb1b4c625e2d
Lisbeth Stafferton <131831523+lisbethS4W0art9studio@users.noreply.github.com>
2023-08-09 14:10:40 +1000

    updated java

--- src/script.js
+++ src/script.js
@@ -83,7 +83,6 @@ function displayCelsiusTemperature(event) {
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
-let celsiusTemperature = null;
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
