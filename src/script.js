30f5d73cdc17398301522f2330f87708532093d6
Lisbeth Stafferton <131831523+lisbethS4W0art9studio@users.noreply.github.com>
2023-08-13 14:42:54 +1000

    Moved HTML forecast code into Javascript and added forecast code into JS

--- src/script.js
+++ src/script.js
@@ -30,6 +30,32 @@ function formatDay(timestamp) {
   return days[day];
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+
+  let forecastHTML = '<div class="row">';
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col-2">
+              <div class="weather-forecast-date">Thursday</div>
+              <img src="https://openweathermap.org/img/wn/50n@2x.png" alt="" width="36px" />
+              <div class="weather-forecast-temperatures">
+                <span class="weather-forecast-temperature-max">18°C </span>
+                <span class="weather-forecast-temerature-min">12°C</span>
+              </div>
+            </div>
+         `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+  console.log(forecastHTML);
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -94,3 +120,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Hobart");
+displayForecast();
